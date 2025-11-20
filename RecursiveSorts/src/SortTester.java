54b9512da58e49d68db65f9293fe62193ecf9916
krishs-23 <krshsenthil@gmail.com>
2025-11-19 17:37:28 -0800

    Added test cases w/ comments to Sort Tester file for Quick sort class

--- RecursiveSorts/src/SortTester.java
+++ RecursiveSorts/src/SortTester.java
@@ -7,8 +7,8 @@
  * Description: Test class for my recursive sorting algorithms (merge sort & quick sort)
  */
 
-import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 
 public class SortTester {
 	// Merge Sort Tests
@@ -100,8 +100,87 @@ public class SortTester {
     // Quick Sort Tests
     
     /**
-     * 
+	 * Tests Quick Sort algorithm with a standard unsorted array
+	 */
+	@Test
+	public void testQuickSortStandard() {
+		int[] nums = {5, 1, 4, 2};
+        int[] expected = {1, 2, 4, 5};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+	}
+	
+	/**
+     * Tests Quick Sort algorithm with a reversed array
      */
+    @Test
+    public void testQuickSortReversed() {
+        int[] nums = {4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+    }
+
+    /**
+	 * Tests Quick Sort algorithm with an already sorted array
+	 */
+    @Test
+    public void testQuickSortAlreadySorted() {
+        int[] nums = {1, 2, 3, 4};
+        int[] expected = {1, 2, 3, 4};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+    }
     
+    /**
+     * Tests Quick Sort algorithm with an empty array
+     */
+    @Test
+    public void testQuickSortEmpty() {
+        int[] nums = {};
+        int[] expected = {};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+    }
+
+    /**
+	 * Tests Quick Sort algorithm with an array with just one element
+	 */
+    @Test
+    public void testQuickSortSingle() {
+        int[] nums = {7};
+        int[] expected = {7};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+    }
+    
+    /**
+	 * Tests Quick Sort algorithm with an array containing duplicates
+	 */
+    @Test
+    public void testQuickSortDuplicates() {
+        int[] nums = {5, 2, 5, 1};
+        int[] expected = {1, 2, 5, 5};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+    }
+    
+    /**
+     * Tests Quick Sort algorithm with an array of an odd size
+     */
+    @Test
+    public void testQuickSortOddSizeArray() {
+    	int[] nums = {5, 2, 5, 1, 13, 9, 47};
+        int[] expected = {1, 2, 5, 5, 9, 13, 47};
+        QuickSort.sort(nums);
+        assertArrayEquals(expected, nums);
+        System.out.println();
+    }
 	
 }
